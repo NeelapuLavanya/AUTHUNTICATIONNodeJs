6fdb1d2f8def954de80bc9dbb92a4f259d7ac2df
lavanya <lavanyahaima@gmail.com>
2023-09-12 12:27:04 +0000

    solution

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ app.post("/register", async (request, response) => {
 app.post("/login", async (request, response) => {
   const { username, password } = request.body;
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
+  const databaseUser = await database.get(selectUserQuery);
   if (databaseUser === undefined) {
     response.status(400);
     response.send("Invalid User");
